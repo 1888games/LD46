7f4ef6f2ad41bf5864bd8c06e6a04564a0ddd426
1888games <nick@1888games.co.uk>
2020-04-14 20:19:46 +0100

    Skeleton project

---
+++ Assets/Scripts/Controllers/SoundController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class SoundController : MonoBehaviourSingleton<SoundController>
+{
+
+	public List<InAudioNode> Nodes;
+
+	public Dictionary<string, InAudioNode> Sounds;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+
+		Sounds = new Dictionary<string, InAudioNode> ();
+
+		foreach (InAudioNode node in Nodes) {
+
+			Sounds.Add (node.Name, node);
+			
+		}
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+	public async void PlaySound (string name, int delay = 0, float volume = 1f) {
+
+		if (Sounds.ContainsKey (name)) {
+
+	
+			InAudio.SetVolumeForAudioFolder (Sounds [name], volume);
+		
+		
+
+			await Task.Delay (delay);
+
+			if (volume < 1f) {
+				InAudio.Play (Camera.main.gameObject, Sounds [name], new AudioParameters (volume, Random.Range (0.8f, 1.1f), 0, 1));
+
+			} else {
+				InAudio.Play (Camera.main.gameObject, Sounds [name]);
+			}
+		
+		}
+
+
+	}
+}
